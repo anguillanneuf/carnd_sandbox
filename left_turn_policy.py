bd80e09e068484a0f8338ac16bce7f9b5ff91a86
Tianzi <tianzih@outlook.com>
2017-07-17 16:55:33 -0400

    left_turn_policy donegit add *git add *

--- left_turn_policy.py
+++ left_turn_policy.py
@@ -30,8 +30,8 @@ action_name = ['R', '#', 'L']
 grid = [[1, 1, 1, 0, 0, 0],
         [1, 1, 1, 0, 1, 0],
         [0, 0, 0, 0, 0, 0],
-        [1, 1, 1, 0, 1, 1],
-        [1, 1, 1, 0, 1, 1]]
+        [0, 1, 1, 0, 1, 1],
+        [0, 0, 0, 0, 1, 1]]
 
 init = [4, 3, 0]  # given in the form [row,col,direction]
 # direction = 0: up
@@ -39,7 +39,7 @@ init = [4, 3, 0]  # given in the form [row,col,direction]
 #             2: down
 #             3: right
 
-goal = [2, 0]  # given in the form [row,col]
+goal = [3, 0]  # given in the form [row,col]
 
 cost = [2, 1, 20]  # cost has 3 values, corresponding to making
 
@@ -60,97 +60,100 @@ cost = [2, 1, 20]  # cost has 3 values, corresponding to making
 # ----------------------------------------
 
 def optimum_policy2D(grid, init, goal, cost):
+	
+	closed = [[[0,0,0,0] if gg==0 else [1,1,1,1] for gg in g] for g in grid]
+	
+	x = init[0]
+	y = init[1]
+	z = init[2] # orientation / direction
+	g = 0
+	open = [[g, x, y, z]]
+	found = False  # flag that is set when search is complete
+	resign = False  # flag set if we can't find expand
 
-    closed = [[[0,0,0,0] if gg==0 else [1,1,1,1] for gg in g] for g in grid]
+	
 
-    x = init[0]
-    y = init[1]
-    z = init[2]
-    g = 0
+	cache = []
+	while not found and not resign: 
+		
+		if len(open) == 0:
+			resign = True
+			return 'fail'
+			
+#		elif all([c for a in closed for b in a for c in b]) == 1:
+#			exhausted = True
+			
+		else:
 
-    open = [[g, x, y, z]]
+			open.sort()
+			open.reverse()
+			next = open.pop()
+			
+			x = next[1]
+			y = next[2]
+			z = next[3]
+			g = next[0]
+			
+#			print("looking at {} from {}".format(next, open_))
+			
+			if x==goal[0] and y==goal[1]:
+#				print("found")
+				found = True
+			else:
+				found = False
+				
+				for i in range(len(forward)):
+					
+					x2 = x + forward[i][0]
+					y2 = y + forward[i][1]
+					z2 = i
+					
+					if z2 - z in [1, -3]:
+						a = 2 # left
+					elif z2 - z in [-1, 3]:
+						a = 0 # right
+					elif z2 == z:
+						a = 1 # straight
+					else:
+						continue # impossible move
+					
+					g2 = g + cost[a]
+					
+					if x2 >= 0 and x2 < len(grid) and y2 >= 0 and y2 < len(grid[0]):
+						if (closed[x2][y2][z2] == 0 or closed[x2][y2][z2] > g2) and grid[x2][y2] == 0:
+#							print("[{},{},{}] to [{},{},{}] can be done".format(x,y,z,x2,y2,z2))
+							open.append([g2, x2, y2, z2])
+							closed[x2][y2][z2] = g2
+							landed = [g, x, y, z, action_name[a], g2, x2, y2, z2]
+#							print(landed)
+							cache.append(landed)
+#							print("\n")
+	
+#	print("out of while loop")
+#	print(cache)
+	
+	path = []
+	dest = min([c[5:] for c in cache if c[6:8]==goal])						
+#	print(dest)
+	curr = [0]+init
+#	print(curr)
+	
+	while dest != curr:
+		for k in cache:
+			if k[5:] == dest:
+				path.append(k)
+				dest = k[:4]
 
-    found = False  # flag that is set when search is complete
-    resign = False  # flag set if we can't find expand
-    optimal = False
-
-    cnt = 0
-    cache = []
-    min_cost = 17
-
-
-    while not found and not resign and not optimal:
-
-        if len(open) == 0:
-            resign = True
-            return 'fail'
-        else:
-            open.sort()
-            open.reverse()
-            next = open.pop()
-            x = next[1]
-            y = next[2]
-            z = next[3]
-            g = next[0]
-
-            if x == goal[0] and y == goal[1]:
-                found = True
-            else:
-
-                if all([c for a in closed for b in a for c in b]) == 1: # check
-
-                    optimal = True
-
-                else:
-
-                    for i in range(len(forward)):
-                        x2 = x + forward[i][0]
-                        y2 = y + forward[i][1]
-                        z2 = i
-
-                        if z2 == z:
-                            a = 1 # forward
-                        elif z2-z in [1, -3]:
-                            a = 2 # left
-                        else:
-                            a = 0 # right
-
-
-                        if x2 >= 0 and x2 < len(grid) and y2 >= 0 and y2 < len(grid[0]):
-
-                            if closed[x2][y2][z2]==0 and grid[x2][y2] == 0:
-
-                                g2 = g + cost[a]
-                                old = min(closed[x2][y2])
-
-                                if g2 < old or old == 0:
-                                    open.append([g2, x2, y2, z2])
-                                print([g2, x2, y2, z2])
-                                closed[x2][y2][z2] = 1
-
-                                # save path.
-                                landed = [x, y, z, x2, y2, z2, a, action_name[a]]
-                                cache.append(landed)
-
-    path = []
-    dest = init
-    print(cache)
-
-    while dest != goal+[2]:
-        for k in cache:
-            if k[:3] == dest:
-                path.append(k)
-                dest = k[3:6]
-    # print(path)
-
-    expand = [[' ' for row in range(len(grid[0]))] for col in range(len(grid))]
-    expand[goal[0]][goal[1]] = '*'
-    for p in path:
-        expand[p[0]][p[1]] = p[7]
-
-    policy2D = expand
-    return policy2D
-
+	expand = [[' ' for row in range(len(grid[0]))] for col in range(len(grid))]
+	expand[goal[0]][goal[1]] = '*'
+	
+#	print(path)
+	for p in path:
+		expand[p[1]][p[2]] = p[4]
+	policy2D = expand
+	
+	return policy2D
+	
 p = optimum_policy2D(grid, init, goal, cost)
 for i in p:
     print(i)
